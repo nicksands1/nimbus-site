eb8cf2fb0bfc8c81c90d29de04e139f412cb19af
nicksands1 <nick.sanders41@gmail.com>
2025-05-02 14:24:02 -0500

    fix intro

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Tag from './Tag'
 
-const text = `Nimbus transforms assets into functional communities where token ownership grants access to exclusive content and experiences. Nimbus establishes a new standard of legitimacy in the crypto space—one where communities unite around genuine utility rather than hype cycles and empty promises.`
+const text = `Nimbus transforms assets into functional communities where token owners gain access to exclusive content and experiences. Nimbus is the new standard of legitimacy in the crypto space—one where communities unite around genuine utility rather than hype cycles and empty promises.`
 
 const Introduction = () => {
   return (
@@ -11,7 +11,7 @@ const Introduction = () => {
             <Tag>Introducing Nimbus</Tag>
         </div>
         <div className='text-4xl text-center font-medium mt-10'>
-            <span>Nimbus eliminates FUD by providing immediate utility through customizable web3 experiences.</span>{" "}
+            <span>Nimbus provides immediate utility with customizable web3 experiences.</span>{" "}
             <span className='text-white/15'>{text}</span>
             <span className='text-yellow-400 block'>That&apos;s why we built Nimbus</span>
         </div>
