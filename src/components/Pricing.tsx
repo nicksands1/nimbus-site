003df73f0e93f59566f5d755d3a56806d730414a
nicksands1 <nick.sanders41@gmail.com>
2025-05-02 16:57:47 -0500

    remove padding

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -50,7 +50,7 @@ export const pricingTiers = [
   ];
   
   export const Pricing = () => {
-    return <section className="pb-[1000px]">
+    return <section className="">
         <div className="container">
             <SectionBorder>
                 <SectionContent>
